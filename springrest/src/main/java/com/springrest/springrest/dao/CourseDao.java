833abf6d3fc3150929609d1ee9f026ba9c42d37d
riyazkhan21 <riyazuddin.khan57@gmail.com>
2021-07-01 03:31:23 +0530

    new changes

--- springrest/src/main/java/com/springrest/springrest/dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.springrest.springrest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springrest.springrest.entity.Cource;
-
-public interface CourseDao extends JpaRepository<Cource, Long>{
-
-}
